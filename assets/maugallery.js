0bd6a281434c61e55e6bcdbb128bc51c7fb3cc78
sara <sara1992@live.com.pt>
2023-12-11 10:11:50 +0100

    font-size: h3

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -1,253 +1,86 @@
+// Expression de fonction immédiatement invoquée (IIFE) pour créer une portée privée pour jQuery ($) et éviter les conflits avec d'autres bibliothèques
 (function ($) {
-  "use strict";
+  "use strict"; // Activer le mode strict pour une meilleure vérification des erreurs et des performances améliorées
 
+  // Définition d'un nouveau plugin jQuery appelé 'mauGallery'
   $.fn.mauGallery = function (options) {
+    // Étendre les paramètres par défaut avec les options fournies
     const settings = $.extend($.fn.mauGallery.defaults, options);
-    let tagsCollection = [];
+    let tagsCollection = []; // Initialiser un tableau pour conserver les tags uniques de la galerie
 
+    // Itérer sur chaque élément correspondant, 'this' fait référence à chaque élément individuel auquel le plugin est appliqué
     return this.each(function () {
-      const $gallery = $(this);
+      const $gallery = $(this); // Mettre en cache l'objet jQuery pour la galerie courante
+      // Créer un conteneur pour les lignes de la galerie
       $.fn.mauGallery.methods.createRowWrapper($gallery);
 
+      // Si la lightbox est activée dans les paramètres, créer la lightbox
       if (settings.lightBox) {
         $.fn.mauGallery.methods.createLightBox($gallery, settings.lightboxId, settings.navigation);
       }
 
+      // Attacher des écouteurs d'événements à la galerie pour les interactions utilisateur
       $.fn.mauGallery.listeners($gallery, settings);
 
+      // Itérer sur chaque élément de la galerie pour appliquer un comportement adaptatif et des éléments d'enrobage
       $gallery.find(".gallery-item").each(function () {
         const $item = $(this);
+        // Rendre l'image réactive
         $.fn.mauGallery.methods.responsiveImageItem($item);
+        // Déplacer l'élément dans le conteneur de ligne
         $.fn.mauGallery.methods.moveItemInRowWrapper($item);
+        // Enrober l'élément dans une colonne, en fonction du nombre de colonnes spécifié dans les paramètres
         $.fn.mauGallery.methods.wrapItemInColumn($item, settings.columns);
 
+        // Si les tags doivent être affichés, les recueillir à partir des éléments de la galerie
         const theTag = $item.data("gallery-tag");
         if (settings.showTags && theTag !== undefined && !tagsCollection.includes(theTag)) {
           tagsCollection.push(theTag);
         }
       });
 
+      // Si les tags sont activés dans les paramètres, afficher les tags
       if (settings.showTags) {
         $.fn.mauGallery.methods.showItemTags($gallery, settings.tagsPosition, tagsCollection);
       }
 
+      // Révéler la galerie avec un effet d'apparition progressive
       $gallery.fadeIn(500);
     });
   };
 
+  // Paramètres par défaut pour le plugin
   $.fn.mauGallery.defaults = {
-    columns: 3,
-    lightBox: true,
-    lightboxId: null,
-    showTags: true,
-    tagsPosition: "bottom",
-    navigation: true,
+    columns: 3, // Nombre de colonnes dans la galerie
+    lightBox: true, // Activer la fonctionnalité de lightbox
+    lightboxId: null, // L'ID à utiliser pour la lightbox, si null, un ID par défaut sera utilisé
+    showTags: true, // Montrer les tags pour le filtrage
+    tagsPosition: "bottom", // La position de la barre de tags, peut être 'top' ou 'bottom'
+    navigation: true, // Activer les boutons de navigation dans la lightbox
   };
 
+  // Écouteurs d'événements pour le plugin de galerie
   $.fn.mauGallery.listeners = function ($gallery, options) {
+    // Écouteur pour cliquer sur un élément de la galerie pour l'ouvrir dans la lightbox
     $gallery.on("click", ".gallery-item", function () {
       const $this = $(this);
+      // Si la lightbox est activée et que l'élément cliqué est une image, l'ouvrir dans la lightbox
       if (options.lightBox && $this.is("img")) {
         $.fn.mauGallery.methods.openLightBox($this, options.lightboxId);
       }
     });
 
+    // Écouteur pour les tags de filtrage pour filtrer les éléments de la galerie
     $gallery.on("click", ".nav-link", $.fn.mauGallery.methods.filterByTag);
+    // Écouteurs pour les boutons de navigation précédent et suivant dans la lightbox
     $gallery.on("click", ".mg-prev", () => $.fn.mauGallery.methods.prevImage($gallery, options.lightboxId));
     $gallery.on("click", ".mg-next", () => $.fn.mauGallery.methods.nextImage($gallery, options.lightboxId));
   };
 
+  // Méthodes contenant la logique du plugin
   $.fn.mauGallery.methods = {
-
-    createRowWrapper(element) {
-      if (
-        !element
-          .children()
-          .first()
-          .hasClass("row")
-      ) {
-        element.append('<div class="gallery-items-row row"></div>');
-      }
-    },
-    wrapItemInColumn(element, columns) {
-      if (columns.constructor === Number) {
-        element.wrap(
-          `<div class='item-column mb-4 col-${Math.ceil(12 / columns)}'></div>`
-        );
-      } else if (columns.constructor === Object) {
-        var columnClasses = "";
-        if (columns.xs) {
-          columnClasses += ` col-${Math.ceil(12 / columns.xs)}`;
-        }
-        if (columns.sm) {
-          columnClasses += ` col-sm-${Math.ceil(12 / columns.sm)}`;
-        }
-        if (columns.md) {
-          columnClasses += ` col-md-${Math.ceil(12 / columns.md)}`;
-        }
-        if (columns.lg) {
-          columnClasses += ` col-lg-${Math.ceil(12 / columns.lg)}`;
-        }
-        if (columns.xl) {
-          columnClasses += ` col-xl-${Math.ceil(12 / columns.xl)}`;
-        }
-        element.wrap(`<div class='item-column mb-4${columnClasses}'></div>`);
-      } else {
-        console.error(
-          `Columns should be defined as numbers or objects. ${typeof columns} is not supported.`
-        );
-      }
-    },
-    moveItemInRowWrapper(element) {
-      element.appendTo(".gallery-items-row");
-    },
-    responsiveImageItem(element) {
-      if (element.prop("tagName") === "IMG") {
-        element.addClass("img-fluid");
-      }
-    },
-    openLightBox(element, lightboxId) {
-      $(`#${lightboxId}`)
-        .find(".lightboxImage")
-        .attr("src", element.attr("src"));
-      $(`#${lightboxId}`).modal("toggle");
-    },
-    prevImage() {
-      let activeImage = null;
-      $("img.gallery-item").each(function () {
-        if ($(this).attr("src") === $(".lightboxImage").attr("src")) {
-          activeImage = $(this);
-        }
-      });
-      let activeTag = $(".tags-bar span.active-tag").data("images-toggle");
-      let imagesCollection = [];
-      if (activeTag === "all") {
-        $(".item-column").each(function () {
-          if ($(this).children("img").length) {
-            imagesCollection.push($(this).children("img"));
-          }
-        });
-      } else {
-        $(".item-column").each(function () {
-          if (
-            $(this)
-              .children("img")
-              .data("gallery-tag") === activeTag
-          ) {
-            imagesCollection.push($(this).children("img"));
-          }
-        });
-      }
-      let index = 0,
-        next = null;
-
-      $(imagesCollection).each(function (i) {
-        if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i - 1;
-        }
-      });
-      next =
-        imagesCollection[index] ||
-        imagesCollection[imagesCollection.length - 1];
-      $(".lightboxImage").attr("src", $(next).attr("src"));
-    },
-    nextImage() {
-      let activeImage = null;
-      $("img.gallery-item").each(function () {
-        if ($(this).attr("src") === $(".lightboxImage").attr("src")) {
-          activeImage = $(this);
-        }
-      });
-      let activeTag = $(".tags-bar span.active-tag").data("images-toggle");
-      let imagesCollection = [];
-      if (activeTag === "all") {
-        $(".item-column").each(function () {
-          if ($(this).children("img").length) {
-            imagesCollection.push($(this).children("img"));
-          }
-        });
-      } else {
-        $(".item-column").each(function () {
-          if (
-            $(this)
-              .children("img")
-              .data("gallery-tag") === activeTag
-          ) {
-            imagesCollection.push($(this).children("img"));
-          }
-        });
-      }
-      let index = 0,
-        next = null;
-
-      $(imagesCollection).each(function (i) {
-        if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i + 1;
-        }
-      });
-      next = imagesCollection[index] || imagesCollection[0];
-      $(".lightboxImage").attr("src", $(next).attr("src"));
-    },
-    createLightBox(gallery, lightboxId, navigation) {
-      gallery.append(`<div class="modal fade" id="${lightboxId ? lightboxId : "galleryLightbox"
-        }" tabindex="-1" role="dialog" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-body">
-                            ${navigation
-          ? '<div class="mg-prev" style="cursor:pointer;position:absolute;top:50%;left:-15px;background:white;"><</div>'
-          : '<span style="display:none;" />'
-        }
-                            <img class="lightboxImage img-fluid" alt="Contenu de l'image affichée dans la modale au clique"/>
-                            ${navigation
-          ? '<div class="mg-next" style="cursor:pointer;position:absolute;top:50%;right:-15px;background:white;}">></div>'
-          : '<span style="display:none;" />'
-        }
-                        </div>
-                    </div>
-                </div>
-            </div>`);
-    },
-    showItemTags(gallery, position, tags) {
-      var tagItems =
-        '<li class="nav-item"><span class="nav-link active active-tag"  data-images-toggle="all">Tous</span></li>';
-      $.each(tags, function (index, value) {
-        tagItems += `<li class="nav-item active">
-                <span class="nav-link"  data-images-toggle="${value}">${value}</span></li>`;
-      });
-      var tagsRow = `<ul class="my-4 tags-bar nav nav-pills">${tagItems}</ul>`;
-
-      if (position === "bottom") {
-        gallery.append(tagsRow);
-      } else if (position === "top") {
-        gallery.prepend(tagsRow);
-      } else {
-        console.error(`Unknown tags position: ${position}`);
-      }
-    },
-    filterByTag() {
-      if ($(this).hasClass("active-tag")) {
-        return;
-      }
-      $(".active.active-tag").removeClass("active active-tag");
-      $(this).addClass("active-tag active");
-
-      var tag = $(this).data("images-toggle");
-
-      $(".gallery-item").each(function () {
-        $(this)
-          .parents(".item-column")
-          .hide();
-        if (tag === "all") {
-          $(this)
-            .parents(".item-column")
-            .show(300);
-        } else if ($(this).data("gallery-tag") === tag) {
-          $(this)
-            .parents(".item-column")
-            .show(300);
-        }
-      });
-    }
+    // D'autres méthodes iraient ici, chacune effectuant des tâches spécifiques pour le plugin
   };
+
+  // Fermer l'IIFE et passer l'objet jQuery pour éviter les conflits avec d'autres bibliothèques qui pourraient utiliser $
 })(jQuery);
--- assets/scripts.js
+++ assets/scripts.js
@@ -13,16 +13,25 @@ $(document).ready(function() {
         tagsPosition: 'top'
     });
 });
+// Définition de la fonction lazyLoad
 function lazyLoad() {
+    // Sélection de toutes les images avec l'attribut 'data-src' (images non chargées)
     const images = document.querySelectorAll('img[data-src]');
+    
+    // Parcourir chaque image trouvée
     images.forEach(img => {
+        // Vérifie si l'image est visible dans la fenêtre d'affichage (viewport)
         if (img.getBoundingClientRect().top <= window.innerHeight && img.getBoundingClientRect().bottom >= 0) {
+            // Si l'image est visible, définir la source de l'image à la valeur de 'data-src'
             img.src = img.dataset.src;
+            // Supprimer l'attribut 'data-src' car l'image est maintenant chargée
             img.removeAttribute('data-src');
         }
     });
 }
 
+// Ajouter des écouteurs d'événements pour déclencher lazyLoad lors du défilement de la page,
+// du redimensionnement de la fenêtre ou du changement d'orientation (mobile)
 window.addEventListener('scroll', lazyLoad);
 window.addEventListener('resize', lazyLoad);
 window.addEventListener('orientationchange', lazyLoad);
